709cb120de40c259802f9e74d4f29c5cc1258904
ankittech <ankitech@gmail.com>
2020-04-21 23:15:55 +0530

    initial commit

---
+++ src/main/java/ankitech/dockersetup/DockerSetupApplication.java
@@ -0,0 +1,13 @@
+package ankitech.dockersetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ankitech/dockersetup/DockerSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package ankitech.dockersetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
